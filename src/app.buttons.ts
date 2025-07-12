22ed0cc0e291c02ad5eaccadf9044588a8daaaa3
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-07-12 17:39:19 +0500

    helmet and rate-limit added

--- src/app.buttons.ts
+++ src/app.buttons.ts
@@ -1,19 +1,9 @@
 import { Markup } from "telegraf";
 
 export function actionButtons(): ReturnType<typeof Markup.keyboard> {
-  return Markup.keyboard(
-    // [
-    //   Markup.button.callback("Topshiriqlar to'plami 📋", "list"),
-    //   Markup.button.callback("Yaratish 📝", 'create'),
-    //   Markup.button.callback("Yakunlash ✅", "done"),
-    //   Markup.button.callback("O'zgartirish ✏️", "edit"),
-    //   Markup.button.callback("O'chirish ❌", "delete"),
-    // ],
-    // { columns: 2 },
-    [
-      ["Topshiriqlar to'plami 📋"],
-      ["Yaratish 📝", "Yakunlash ✅"],
-      ["O'zgartirish ✏️", "O'chirish ❌"],
-    ],
-  ).resize();
+  return Markup.keyboard([
+    ["Topshiriqlar to'plami 📋"],
+    ["Yaratish 📝", "Yakunlash ✅"],
+    ["O'zgartirish ✏️", "O'chirish ❌"],
+  ]).resize();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 60 * 1000,
+      max: 60,
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
